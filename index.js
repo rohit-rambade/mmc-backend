ba1e49c4d255b04b1b88aab3d501e9e598bd295c
Rohit <rohitrambade18@gmail.com>
2023-08-14 23:07:22 +0530

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const pool = require("./db");
+
+//middlewares
+app.use(express.json());
+
+//Routes
+app.post("/api/test", async (req, res) => {
+    try {
+        const { firstname, lastname } = req.body;
+        const client = await pool.connect();
+        const query = `
+          INSERT INTO users (firstname, lastname)
+          VALUES ($1, $2)
+          RETURNING user_id;
+        `;
+        const values = [firstname, lastname];
+        const result = await client.query(query, values);
+        const user_id = result.rows[0].user_id;
+        client.release();
+
+        res.status(201).json({ message: 'User data submitted successfully', user_id });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'An error occurred' });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server started");
+});
